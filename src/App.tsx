b6282d9c08e1de397f1078048e1589224c534a46
Hershi Shor <e0548597918@gmail.com>
2025-09-03 19:38:44 +0300

    store the token in localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
 import CreatePlayer from "./components/CreateRiddle";
 import LogInPage from "./pages/LogInPage";
+import { useNavigate } from "react-router";
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/play");
+    }
+  });
   return (
     <>
       <h1>Hi</h1>
